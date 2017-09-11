6312fe6c8bd56eb3fbf6d65d94525347c8bde210
P Bellefleur <bellefleurpeter@yahoo.ca>
2017-09-11 13:25:10 -0400

    Initial commit

---
+++ app/src/main/java/ca/qc/dawsoncollege/a1132371/loancalculator/MainActivity.java
@@ -0,0 +1,14 @@
+package ca.qc.dawsoncollege.a1132371.loancalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    } //onCreate() 
+}
